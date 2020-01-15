a1239327f2617bcd1f108a31129b61a74f8bf74f
Shakti Chouhan <shachouh@publicisgroupe.net>
2020-01-15 14:40:41 +0530

    Fix typo

--- client/components/Overview/CallToAction/Actions.js
+++ client/components/Overview/CallToAction/Actions.js
@@ -4,12 +4,12 @@ import './Actions.css';
 
 const CallToAction = () =>{
     return (
-        <div className="offering_action_continer">
+        <div className="offering_action_container">
             <h1>ADVERTISE ON ADWORKSCOM</h1>
             <h2>GEO-TARGETED| MEMORABLE| AFFORDABLE</h2>
             <div className="offering_action_ctadiv">
-            <Button outline color="info" className="offergin_action_ctagroup">Create Ad</Button>
-            <Button outline color="info" className="offergin_action_ctagroup">Contact Us</Button>
+            <Button outline color="info" className="offering_action_ctagroup">Create Ad</Button>
+            <Button outline color="info" className="offering_action_ctagroup">Contact Us</Button>
             </div>
         </div>
     );
