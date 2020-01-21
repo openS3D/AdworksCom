55eec7305e2945e7aac76964cac46f94a7e105e6
Shakti Chouhan <shachouh@publicisgroupe.net>
2020-01-21 23:36:50 +0530

    Add cell number to website

--- client/hoc/layout/footer/footer.js
+++ client/hoc/layout/footer/footer.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './footer.css';
 
 const Footer = () => {
-    return(
+    return (
         <p className="footer_container">
             All rights reserved by S3D group
+            <p>
+                Call us at <a href="tel:+1-303-499-7111">+1 (303) 499-7111</a>
+            </p>
         </p>
     );
 }
